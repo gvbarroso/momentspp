bea8accb23d69e9de9c42ed3ab4ad1dd7c9021b4
Gustavo V. Barroso <gvbarroso@gmail.com>
2022-09-22 09:49:08 -0500

    forgot to add Data class

---
+++ src/PolymorphismData.cpp
@@ -0,0 +1,20 @@
+/*
+ * Authors: Gustavo V. Barroso
+ * Created: 22/09/2022
+ * Last modified: 22/09/2022
+ *
+ */
+
+
+#include "PolymorphismData.hpp"
+
+
+void PolymorphismData::parse(const std::string& file)
+{
+
+}
+
+void PolymorphismData::computeSumStats()
+{
+
+}
