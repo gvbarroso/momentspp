ee71f3bfe35569a4eeb789a506ca61b033548f56
Gustavo V. Barroso <gvbarroso@gmail.com>
2023-11-22 16:02:31 -0600

    big improvements in simulator

--- src/TwoLocusPop.hpp
+++ src/TwoLocusPop.hpp
@@ -1,7 +1,7 @@
 /*
  * Authors: Gustavo V. Barroso
  * Created: 24/10/2023
- * Last modified: 09/11/2023
+ * Last modified: 22/11/2023
  */
 
 
@@ -13,8 +13,7 @@
 #include <vector>
 #include <iostream>
 #include <array>
-
-#include "TwoLocusPair.hpp"
+#include <numeric>
 
 class TwoLocusPop
 {
@@ -22,41 +21,75 @@ class TwoLocusPop
 private:
   size_t id_;
   size_t l_; // length of segment
-  unsigned int n_; // haploid pop size
-  std::vector<TwoLocusPair> pairs_;
+  unsigned int ne_; // pop size
+  std::vector<std::array<double, 4>> x_; // in order: AB, Ab, aB, ab
+  std::vector<double> xl_; // allele freqs. at (independent) left locus
+  std::vector<double> xr_; // allele freqs. at (independent) right locus
 
 public:
   TwoLocusPop():
   id_(0),
   l_(1),
-  n_(0),
-  pairs_(0)
+  ne_(1000),
+  x_(0),
+  xl_(0),
+  xr_(0)
   { }
 
-  TwoLocusPop(size_t id, size_t l, size_t n, const std::vector<TwoLocusPair>& pairs):
+  TwoLocusPop(size_t id, size_t l, unsigned int ne):
   id_(id),
   l_(l),
-  n_(n),
-  pairs_(pairs)
-  { }
-
-public:
-  void printAttributes(std::ostream& stream)
+  ne_(ne),
+  x_(0),
+  xl_(0),
+  xr_(0)
   {
-    stream << "Pop. id: " << id_ << " (" << pairs_.size() << " segregating pairs)\n";
-
-    std::array<double, 5> stats = fetchAvgStats();
-
-    stream << "avg Hl = " << stats[0] << ", ";
-    stream << "avg Hr = " << stats[1] << ", ";
-    stream << "avg Pi2 = " << stats[2] << ", ";
-    stream << "avg Dz = " << stats[3] << ", ";
-    stream << "avg D^2 = " << stats[4] << "\n\n";
+    x_.reserve(10 * l);
+    xl_.reserve(10 * l);
+    xr_.reserve(10 * l);
   }
 
-  unsigned int getN()
+public:
+  void printXl()
   {
-    return n_;
+    std::cout << "Xl:\n\t";
+
+    for(auto& v : xl_)
+      std::cout << v << ",";
+
+    std::cout << "\n";
+  }
+
+  void printXr()
+  {
+    std::cout << "Xr:\n\t";
+
+    for(auto& v : xr_)
+      std::cout << v << ",";
+
+    std::cout << "\n";
+  }
+
+  void printX()
+  {
+    for(size_t i = 0; i < x_.size(); ++i)
+    {
+      std::cout << "\tfAB = " << x_[i][0] << ", ";
+      std::cout << "fAb = " << x_[i][1] << ", ";
+      std::cout << "faB = " << x_[i][2] << ", ";
+      std::cout << "fab = " << x_[i][3] << "\n";
+    }
+
+    std::cout << "\n";
+  }
+
+  void printAttributes(std::ostream& stream)
+  {
+    stream << "Pop. id: " << id_ << "\n";
+
+    printXl();
+    printXr();
+    printX();
   }
 
   size_t getId()
@@ -74,194 +107,345 @@ public:
     id_ = id;
   }
 
-  void setPopSize(unsigned int n)
+  void setPopSize(unsigned int ne)
   {
-    n_ = n;
-
-    for(auto it = std::begin(pairs_); it != std::end(pairs_); ++it)
-      it->setPopSize(n);
+    ne_ = ne;
   }
 
-  const std::vector<TwoLocusPair>& getPairs() const
+  const std::vector<std::array<double, 4>>& getX() const
   {
-    return pairs_;
+    return x_;
   }
 
-  std::vector<TwoLocusPair>& getPairs()
+  std::vector<std::array<double, 4>>& getX()
   {
-    return pairs_;
+    return x_;
   }
 
-  // within-population stats
-  std::array<double, 5> fetchAvgStats()
+  const std::vector<double>& getXl() const
   {
-    std::array<double, 5> stats = { 0., 0., 0., 0., 0. };
+    return xl_;
+  }
 
-    if(pairs_.size() > 0)
+  std::vector<double>& getXl()
+  {
+    return xl_;
+  }
+
+  const std::vector<double>& getXr() const
+  {
+    return xr_;
+  }
+
+  std::vector<double>& getXr()
+  {
+    return xr_;
+  }
+
+  std::vector<double> getDs()
+  {
+    std::vector<double> ret(0);
+    ret.reserve(x_.size());
+
+    for(size_t i = 0; i < x_.size(); ++i)
+      ret.emplace_back(x_[i][0] * x_[i][3] - x_[i][1] * x_[i][2]);
+
+    return ret;
+  }
+
+  std::vector<double> getPs()
+  {
+    std::vector<double> ret(0);
+    ret.reserve(x_.size());
+
+    for(size_t i = 0; i < x_.size(); ++i)
+      ret.emplace_back(x_[i][0] + x_[i][1]);
+
+    return ret;
+  }
+
+  std::vector<double> getQs()
+  {
+    std::vector<double> ret(0);
+    ret.reserve(x_.size());
+
+    for(size_t i = 0; i < x_.size(); ++i)
+      ret.emplace_back(x_[i][0] + x_[i][2]);
+
+    return ret;
+  }
+
+  std::vector<double> getHls()
+  {
+    std::vector<double> ret(0);
+    ret.reserve(xl_.size());
+
+    for(size_t i = 0; i < xl_.size(); ++i)
+      ret.emplace_back(xl_[i] * (1. - xl_[i]));
+
+    return ret;
+  }
+
+  std::vector<double> getHrs()
+  {
+    std::vector<double> ret(0);
+    ret.reserve(xr_.size());
+
+    for(size_t i = 0; i < xr_.size(); ++i)
+      ret.emplace_back(xr_[i] * (1. - xr_[i]));
+
+    return ret;
+  }
+
+  std::vector<double> getDsqrs()
+  {
+    std::vector<double> ret(0);
+    ret.reserve(x_.size());
+
+    for(size_t i = 0; i < x_.size(); ++i)
+      ret.emplace_back((x_[i][0] * x_[i][3] - x_[i][1] * x_[i][2]) * (x_[i][0] * x_[i][3] - x_[i][1] * x_[i][2]));
+
+    return ret;
+  }
+
+  std::vector<double> getDzs()
+  {
+    std::vector<double> ret(0);
+    ret.reserve(x_.size());
+
+    for(size_t i = 0; i < x_.size(); ++i)
     {
-      for(auto it = std::begin(pairs_); it != std::end(pairs_); ++it)
-      {
-        stats[0] += it->fetchHl();
-        stats[1] += it->fetchHr();
+      double d = x_[i][0] * x_[i][3] - x_[i][1] * x_[i][2];
+      double p = x_[i][0] + x_[i][1];
+      double q = x_[i][0] + x_[i][2];
 
-        if(it->bothPolymorphic())
+      ret.emplace_back(d * (1. - 2. * p) * (1. - 2. * q));
+    }
+
+    return ret;
+  }
+
+  std::vector<double> getPi2s()
+  {
+    std::vector<double> ret(0);
+    ret.reserve(x_.size());
+
+    for(size_t i = 0; i < x_.size(); ++i)
+    {
+      double p = x_[i][0] + x_[i][1];
+      double q = x_[i][0] + x_[i][2];
+
+      ret.emplace_back(p * (1. - p) * q * (1. - q));
+    }
+
+    return ret;
+  }
+
+  double getSumHl()
+  {
+    std::vector<double> vals = getHls();
+    std::sort(std::begin(vals), std::end(vals));
+    return std::accumulate(std::begin(vals), std::end(vals), 0.);
+  }
+
+  double getSumHr()
+  {
+    std::vector<double> vals = getHrs();
+    std::sort(std::begin(vals), std::end(vals));
+    return std::accumulate(std::begin(vals), std::end(vals), 0.);
+  }
+
+  double getSumDsqr()
+  {
+    std::vector<double> vals = getDsqrs();
+    std::sort(std::begin(vals), std::end(vals));
+    return std::accumulate(std::begin(vals), std::end(vals), 0.);
+  }
+
+  double getSumDz()
+  {
+    std::vector<double> vals = getDzs();
+    std::sort(std::begin(vals), std::end(vals));
+    return std::accumulate(std::begin(vals), std::end(vals), 0.);
+  }
+
+  double getSumPi2()
+  {
+    std::vector<double> vals = getPi2s();
+    std::sort(std::begin(vals), std::end(vals));
+    return std::accumulate(std::begin(vals), std::end(vals), 0.);
+  }
+
+  void recombine(double r)
+  {
+    for(size_t i = 0; i < x_.size(); ++i)
+    {
+      double d = x_[i][0] * x_[i][3] - x_[i][1] * x_[i][2];
+
+      x_[i][0] -= r * d;
+      x_[i][1] += r * d;
+      x_[i][2] += r * d;
+      x_[i][3] -= r * d;
+    }
+  }
+
+  void select(double s)
+  {
+    for(size_t i = 0; i < xl_.size(); ++i)
+      xl_[i] = ((1 + s) * xl_[i]) / (xl_[i] * (1 + s) + 1 - xl_[i]);
+
+    for(size_t i = 0; i < x_.size(); ++i)
+    {
+      //printX();
+      //double before = x_[i][0] + x_[i][1] + x_[i][2] + x_[i][3];
+
+      double p = x_[i][0] + x_[i][1];
+
+      x_[i][0] *= (1. + s * (1. - p));
+      x_[i][1] *= (1. + s * (1. - p));
+      x_[i][2] *= (1. - s * p);
+      x_[i][3] *= (1. - s * p);
+
+      //double after = x_[i][0] + x_[i][1] + x_[i][2] + x_[i][3];
+
+      //std::cout << std::setprecision(12) << "before = " << before << "; after = " << after << "\n";
+      //printX();
+    }
+  }
+
+  void drift(const gsl_rng* gen)
+  {
+    for(size_t i = 0; i < xl_.size(); ++i)
+      xl_[i] = gsl_ran_binomial(gen, xl_[i], 2. * ne_) / (2. * ne_);
+
+    for(size_t i = 0; i < xr_.size(); ++i)
+      xr_[i] = gsl_ran_binomial(gen, xr_[i], 2. * ne_) / (2. * ne_);
+
+    for(size_t i = 0; i < x_.size(); ++i)
+    {
+      unsigned int next[4];
+      double probs[4] = { x_[i][0], x_[i][1], x_[i][2], x_[i][3] };
+
+      gsl_ran_multinomial(gen, 4, 2 * ne_, probs, next);
+
+      x_[i][0] = static_cast<double>(next[0]) / (2. * ne_);
+      x_[i][1] = static_cast<double>(next[1]) / (2. * ne_);
+      x_[i][2] = static_cast<double>(next[2]) / (2. * ne_);
+      x_[i][3] = static_cast<double>(next[3]) / (2. * ne_);
+    }
+  }
+
+  void mutate(double u, const gsl_rng* gen)
+  {
+    // new single mutations at left locus
+    unsigned int num_left_mut = gsl_ran_poisson(gen, 2. * ne_ * u * l_);
+    for(size_t i = 0; i < num_left_mut; ++i)
+      xl_.emplace_back(1. / (2. * ne_));
+
+    // new single mutations at right locus
+    unsigned int num_right_mut = gsl_ran_poisson(gen, 2. * ne_ * u * l_);
+    for(size_t i = 0; i < num_right_mut; ++i)
+      xr_.emplace_back(1. / (2. * ne_));
+
+
+    for(size_t i = 0; i < l_; ++i)
+    {
+      // mutate against segregating left loci in Xl
+      for(size_t j = 0; j < xl_.size(); ++j)
+      {
+        if(gsl_rng_uniform(gen) < 2 * ne_ * u)
         {
-          stats[2] += it->fetchPi2();
-          stats[3] += it->fetchDz();
-          stats[4] += it->fetchDsqr();
+          if(gsl_rng_uniform(gen) < xl_[j]) // falls on Ab background
+          {
+            std::array<double, 4> vals = {1. / (2. * ne_), xl_[j] - 1. / (2. * ne_), 0., 1. - xl_[j]};
+            x_.emplace_back(vals);
+          }
+
+          else // falls on ab background
+          {
+            std::array<double, 4> vals = {0., xl_[j], 1. / (2. * ne_), 1. - xl_[j] - 1. / (2. * ne_)};
+            x_.emplace_back(vals);
+          }
         }
       }
 
-      stats[0] /= l_;
-      stats[1] /= l_;
-      stats[2] /= l_;
-      stats[3] /= l_;
-      stats[4] /= l_;
-    }
+      // mutate against segregating right loci in Xr
+      for(size_t j = 0; j < xr_.size(); ++j)
+      {
+        if(gsl_rng_uniform(gen) < 2 * ne_ * u)
+        {
+          if(gsl_rng_uniform(gen) < xr_[j]) // falls on aB background
+          {
+            std::array<double, 4> vals = {1. / (2. * ne_), 0., xr_[j] - 1. / (2. * ne_), 1. - xr_[j]};
+            x_.emplace_back(vals);
+          }
 
-    return stats;
+          else // falls on ab background
+          {
+            std::array<double, 4> vals = {0., 1. / (2. * ne_), xr_[j], 1. - xr_[j] - 1. / (2. * ne_)};
+            x_.emplace_back(vals);
+          }
+        }
+      }
+    }
   }
 
-  void evolve_random(const gsl_rng* gen, double u, double r, double s)
+  void evolve(double u, double r, double s, const gsl_rng* gen)
   {
-    for(auto it = std::begin(pairs_); it != std::end(pairs_);)
-    {
-      it->evolve_random(gen, u, r, s);
+    recombine(r);
+    select(s);
+    drift(gen);
+    mutate(u, gen);
+  }
 
-      if(it->monomorphic() && it->mutatedBoth())
-        it = pairs_.erase(it);
+  void evolve(TwoLocusPop& other, double m, double u, double r, double s, const gsl_rng* gen)
+  {
+    migrate_(other, m);
+
+    recombine(r);
+    select(s);
+    drift(gen);
+    mutate(u, gen);
+  }
+
+  void cleanup()
+  {
+    // remove fixed or lost in xl_ and xr_
+    for(auto it = std::begin(xl_); it != std::end(xl_);)
+    {
+      if((*it) == 0. || (*it) == 1.)
+        it = xl_.erase(it);
 
       else
         ++it;
     }
 
-    size_t unlinkedMuts = gsl_ran_poisson(gen, l_ * n_ * u);
-
-    for(size_t j = 0; j < unlinkedMuts; ++j)
+    for(auto it = std::begin(xr_); it != std::end(xr_);)
     {
-      double c_ab = n_ - 1;
-      double c_Ab = 0;
-      double c_aB = 0;
-      double c_AB = 0;
-
-      if(gsl_rng_uniform(gen) < 0.5)
-        c_Ab = 1;
-
-      else
-        c_aB = 1;
-
-      TwoLocusPair newPair(c_ab, c_Ab, c_aB, c_AB);
-      pairs_.emplace_back(newPair);
-    }
-  }
-
-  void evolve_random(TwoLocusPop& other, const gsl_rng* gen, double m, double u, double r, double s)
-  {
-    migrate_random_(other, gen, m);
-
-    for(auto it = std::begin(pairs_); it != std::end(pairs_);)
-    {
-      it->evolve_random(gen, u, r, s);
-
-      if(it->monomorphic() && it->mutatedBoth())
-        it = pairs_.erase(it);
+      if((*it) == 0. || (*it) == 1.)
+        it = xr_.erase(it);
 
       else
         ++it;
     }
 
-    size_t unlinkedMuts = gsl_ran_poisson(gen, l_ * n_ * u);
-
-    for(size_t j = 0; j < unlinkedMuts; ++j)
+    // remove any pair with p or q at 0 or 1
+    for(auto it = std::begin(x_); it != std::end(x_);)
     {
-      double c_ab = n_ - 1;
-      double c_Ab = 0;
-      double c_aB = 0;
-      double c_AB = 0;
+      double p = (*it)[0] + (*it)[1];
+      double q = (*it)[0] + (*it)[2];
 
-      if(gsl_rng_uniform(gen) < 0.5)
-        c_Ab = 1;
-
-      else
-        c_aB = 1;
-
-      TwoLocusPair newPair(c_ab, c_Ab, c_aB, c_AB);
-      pairs_.emplace_back(newPair);
-    }
-  }
-
-  void evolve_det(const gsl_rng* gen, double u, double r, double s)
-  {
-    for(auto it = std::begin(pairs_); it != std::end(pairs_);)
-    {
-      it->evolve_det(gen, u, r, s);
-
-      if(it->monomorphic()) // && it->mutatedBoth())
-        it = pairs_.erase(it);
+      if(p == 0. || p == 1. || q == 0. || q == 1.)
+        it = x_.erase(it);
 
       else
         ++it;
-    }
-
-    size_t unlinkedMuts = gsl_ran_poisson(gen, l_ * n_ * u);
-
-    for(size_t j = 0; j < unlinkedMuts; ++j)
-    {
-      double c_ab = n_ - 1;
-      double c_Ab = 0;
-      double c_aB = 0;
-      double c_AB = 0;
-
-      if(gsl_rng_uniform(gen) < 0.5)
-        c_Ab = 1;
-
-      else
-        c_aB = 1;
-
-      TwoLocusPair newPair(c_ab, c_Ab, c_aB, c_AB);
-      pairs_.emplace_back(newPair);
-    }
-  }
-
-  void evolve_det(TwoLocusPop& other, const gsl_rng* gen, double m, double u, double r, double s)
-  {
-    migrate_det_(other, m);
-
-    for(auto it = std::begin(pairs_); it != std::end(pairs_);)
-    {
-      it->evolve_det(gen, u, r, s);
-
-      if(it->monomorphic() && it->mutatedBoth())
-        it = pairs_.erase(it);
-
-      else
-        ++it;
-    }
-
-    size_t unlinkedMuts = gsl_ran_poisson(gen, l_ * n_ * u);
-
-    for(size_t j = 0; j < unlinkedMuts; ++j)
-    {
-      double c_ab = n_ - 1;
-      double c_Ab = 0;
-      double c_aB = 0;
-      double c_AB = 0;
-
-      if(gsl_rng_uniform(gen) < 0.5)
-        c_Ab = 1;
-
-      else
-        c_aB = 1;
-
-      TwoLocusPair newPair(c_ab, c_Ab, c_aB, c_AB);
-      pairs_.emplace_back(newPair);
     }
   }
 
 private:
-  void migrate_random_(TwoLocusPop& other, const gsl_rng* gen, double m);
+  void migrate_(TwoLocusPop& other, double m);
 
-  void migrate_det_(TwoLocusPop& other, double m);
 };
 
 #endif
--- src/twoLocusSim.cpp
+++ src/twoLocusSim.cpp
@@ -1,7 +1,7 @@
 /*
  * Author: Gustavo V. Barroso
  * Created: 20/10/2023
- * Last modified: 21/11/2023
+ * Last modified: 22/11/2023
  * Source code for twoLocusSim
  *
  */
@@ -21,322 +21,8 @@
 
 #include <Bpp/App/BppApplication.h>
 #include <Bpp/App/ApplicationTools.h>
-#include <Bpp/Text/TextTools.h>
 
-// Functions
-double getP(const std::array<double, 4>& haps)
-{
-  return haps[0] + haps[1];
-}
-
-double getQ(const std::array<double, 4>& haps)
-{
-  return haps[0] + haps[2];
-}
-
-std::vector<double> getDs(const std::vector<std::array<double, 4>>& X)
-{
-  std::vector<double> ret(0);
-  ret.reserve(X.size());
-
-  for(size_t i = 0; i < X.size(); ++i)
-    ret.emplace_back(X[i][0] * X[i][3] - X[i][1] * X[i][2]);
-
-  return ret;
-}
-
-std::vector<double> getPs(const std::vector<std::array<double, 4>>& X)
-{
-  std::vector<double> ret(0);
-  ret.reserve(X.size());
-
-  for(size_t i = 0; i < X.size(); ++i)
-    ret.emplace_back(X[i][0] + X[i][1]);
-
-  return ret;
-}
-
-std::vector<double> getQs(const std::vector<std::array<double, 4>>& X)
-{
-  std::vector<double> ret(0);
-  ret.reserve(X.size());
-
-  for(size_t i = 0; i < X.size(); ++i)
-    ret.emplace_back(X[i][0] + X[i][2]);
-
-  return ret;
-}
-
-std::vector<double> getHls(const std::vector<double>& Xl)
-{
-  std::vector<double> ret(0);
-  ret.reserve(Xl.size());
-
-  for(size_t i = 0; i < Xl.size(); ++i)
-    ret.emplace_back(Xl[i] * (1. - Xl[i]));
-
-  return ret;
-}
-
-std::vector<double> getHrs(const std::vector<double>& Xr)
-{
-  std::vector<double> ret(0);
-  ret.reserve(Xr.size());
-
-  for(size_t i = 0; i < Xr.size(); ++i)
-    ret.emplace_back(Xr[i] * (1. - Xr[i]));
-
-  return ret;
-}
-
-std::vector<double> getDsqrs(const std::vector<std::array<double, 4>>& X)
-{
-  std::vector<double> ret(0);
-  ret.reserve(X.size());
-
-  for(size_t i = 0; i < X.size(); ++i)
-    ret.emplace_back((X[i][0] * X[i][3] - X[i][1] * X[i][2]) * (X[i][0] * X[i][3] - X[i][1] * X[i][2]));
-
-  return ret;
-}
-
-std::vector<double> getDzs(const std::vector<std::array<double, 4>>& X)
-{
-  std::vector<double> ret(0);
-  ret.reserve(X.size());
-
-  std::vector<double> ps = getPs(X);
-  std::vector<double> qs = getQs(X);
-  std::vector<double> ds = getDs(X);
-
-  for(size_t i = 0; i < X.size(); ++i)
-    ret.emplace_back(ds[i] * (1. - 2. * ps[i]) * (1. - 2. * qs[i]));
-
-  return ret;
-}
-
-std::vector<double> getPi2s(const std::vector<std::array<double, 4>>& X)
-{
-  std::vector<double> ret(0);
-  ret.reserve(X.size());
-
-  std::vector<double> ps = getPs(X);
-  std::vector<double> qs = getQs(X);
-
-  for(size_t i = 0; i < X.size(); ++i)
-    ret.emplace_back(ps[i] * (1. - ps[i]) * qs[i] * (1. - qs[i]));
-
-  return ret;
-}
-
-double getSumHl(const std::vector<double>& Xl)
-{
-  std::vector<double> vals = getHls(Xl);
-  std::sort(std::begin(vals), std::end(vals));
-  return std::accumulate(std::begin(vals), std::end(vals), 0.);
-}
-
-double getSumHr(const std::vector<double>& Xr)
-{
-  std::vector<double> vals = getHrs(Xr);
-  std::sort(std::begin(vals), std::end(vals));
-  return std::accumulate(std::begin(vals), std::end(vals), 0.);
-}
-
-double getSumDsqr(const std::vector<std::array<double, 4>>& X)
-{
-  std::vector<double> vals = getDsqrs(X);
-  std::sort(std::begin(vals), std::end(vals));
-  return std::accumulate(std::begin(vals), std::end(vals), 0.);
-}
-
-double getSumDz(const std::vector<std::array<double, 4>>& X)
-{
-  std::vector<double> vals = getDzs(X);
-  std::sort(std::begin(vals), std::end(vals));
-  return std::accumulate(std::begin(vals), std::end(vals), 0.);
-}
-
-double getSumPi2(const std::vector<std::array<double, 4>>& X)
-{
-  std::vector<double> vals = getPi2s(X);
-  std::sort(std::begin(vals), std::end(vals));
-  return std::accumulate(std::begin(vals), std::end(vals), 0.);
-}
-
-void recombine(std::vector<std::array<double, 4>>& X, double r)
-{
-  std::vector<double> ds = getDs(X);
-
-  for(size_t i = 0; i < X.size(); ++i)
-  {
-    X[i][0] -= r * ds[i];
-    X[i][1] += r * ds[i];
-    X[i][2] += r * ds[i];
-    X[i][3] -= r * ds[i];
-  }
-}
-
-void select(std::vector<double>& Xl, double s)
-{
-  for(size_t i = 0; i < Xl.size(); ++i)
-    Xl[i] = ((1 + s) * Xl[i]) / (Xl[i] * (1 + s) + 1 - Xl[i]);
-}
-
-void drift(std::vector<std::array<double, 4>>& X, std::vector<double>& Xl, std::vector<double>& Xr, unsigned int Ne, const gsl_rng* gen)
-{
-  for(size_t i = 0; i < Xl.size(); ++i)
-    Xl[i] = gsl_ran_binomial(gen, Xl[i], 2. * Ne) / (2. * Ne);
-
-  for(size_t i = 0; i < Xr.size(); ++i)
-    Xr[i] = gsl_ran_binomial(gen, Xr[i], 2. * Ne) / (2. * Ne);
-
-  unsigned int n = std::round(2. * Ne);
-
-  for(size_t i = 0; i < X.size(); ++i)
-  {
-    unsigned int next[4];
-    double probs[4] = { X[i][0], X[i][1], X[i][2], X[i][3] };
-
-    gsl_ran_multinomial(gen, 4, n, probs, next);
-
-    X[i][0] = static_cast<double>(next[0]) / (2. * Ne);
-    X[i][1] = static_cast<double>(next[1]) / (2. * Ne);
-    X[i][2] = static_cast<double>(next[2]) / (2. * Ne);
-    X[i][3] = static_cast<double>(next[3]) / (2. * Ne);
-  }
-}
-
-void mutate(std::vector<std::array<double, 4>>& X, std::vector<double>& Xl, std::vector<double>& Xr, unsigned int Ne, size_t L, double u, const gsl_rng* gen)
-{
-  // new single mutations at left locus
-  unsigned int num_left_mut = gsl_ran_poisson(gen, 2. * Ne * u * L);
-  for(size_t i = 0; i < num_left_mut; ++i)
-    Xl.emplace_back(1. / (2. * Ne));
-
-  // new single mutations at right locus
-  unsigned int num_right_mut = gsl_ran_poisson(gen, 2. * Ne * u * L);
-  for(size_t i = 0; i < num_right_mut; ++i)
-    Xr.emplace_back(1. / (2. * Ne));
-
-
-  for(size_t i = 0; i < L; ++i)
-  {
-    // mutate against segregating left loci in Xl
-    for(size_t j = 0; j < Xl.size(); ++j)
-    {
-      if(gsl_rng_uniform(gen) < 2 * Ne * u)
-      {
-        if(gsl_rng_uniform(gen) < Xl[j]) // falls on bA background
-        {
-          std::array<double, 4> vals = {1. / (2. * Ne), Xl[j] - 1. / (2. * Ne), 0., 1. - Xl[j]};
-          X.emplace_back(vals);
-        }
-
-        else // falls on ab background
-        {
-          std::array<double, 4> vals = {0., Xl[j], 1. / (2. * Ne), 1. - Xl[j] - 1. / (2. * Ne)};
-          X.emplace_back(vals);
-        }
-      }
-    }
-
-    // mutate against segregating right loci in Xr
-    for(size_t j = 0; j < Xr.size(); ++j)
-    {
-      if(gsl_rng_uniform(gen) < 2 * Ne * u)
-      {
-        if(gsl_rng_uniform(gen) < Xr[j]) // falls on bA background
-        {
-          std::array<double, 4> vals = {1. / (2. * Ne), Xr[j] - 1. / (2. * Ne), 0., 1. - Xr[j]};
-          X.emplace_back(vals);
-        }
-
-        else // falls on ab background
-        {
-          std::array<double, 4> vals = {0., 1. / (2. * Ne), Xr[j], 1. - Xr[j] - 1. / (2. * Ne)};
-          X.emplace_back(vals);
-        }
-      }
-    }
-  }
-}
-
-void evolve(std::vector<std::array<double, 4>>& X, std::vector<double>& Xl, std::vector<double>& Xr, unsigned int Ne, size_t L, double u, double r, double s, const gsl_rng* gen)
-{
-  recombine(X, r);
-  select(Xl, s);
-  drift(X, Xl, Xr, Ne, gen);
-  mutate(X, Xl, Xr, Ne, L, u, gen);
-}
-
-void cleanup(std::vector<std::array<double, 4>>& X, std::vector<double>& Xl, std::vector<double>& Xr)
-{
-  // remove fixed or lost in Xl and Xr
-  for(auto it = std::begin(Xl); it != std::end(Xl);)
-  {
-    if((*it) == 0. || (*it) == 1.)
-      it = Xl.erase(it);
-
-    else
-      ++it;
-  }
-
-  for(auto it = std::begin(Xr); it != std::end(Xr);)
-  {
-    if((*it) == 0. || (*it) == 1.)
-      it = Xr.erase(it);
-
-    else
-      ++it;
-  }
-
-  // remove any with p or q at 0 or 1
-  for(auto it = std::begin(X); it != std::end(X);)
-  {
-    double p = getP(*it);
-    double q = getQ(*it);
-
-    if(p == 0. || p == 1. || q == 0. || q == 1.)
-      it = X.erase(it);
-
-    else
-      ++it;
-  }
-}
-
-void printXl(const std::vector<double>& Xl)
-{
-  std::cout << "Xl:\n\t";
-
-  for(auto& v : Xl)
-    std::cout << v << ",";
-
-  std::cout << "\n";
-}
-
-void printXr(const std::vector<double>& Xr)
-{
-  std::cout << "Xr:\n\t";
-
-  for(auto& v : Xr)
-    std::cout << v << ",";
-
-  std::cout << "\n";
-}
-
-void printX(const std::vector<std::array<double, 4>>& X)
-{
-  for(size_t i = 0; i < X.size(); ++i)
-  {
-    std::cout << "\tfAB = " << X[i][0] << ", ";
-    std::cout << "fAb = " << X[i][1] << ", ";
-    std::cout << "faB = " << X[i][2] << ", ";
-    std::cout << "fab = " << X[i][3] << "\n";
-  }
-
-  std::cout << "\n";
-}
+#include "TwoLocusPop.hpp"
 
 int main(int argc, char *argv[]) {
 
@@ -351,7 +37,7 @@ int main(int argc, char *argv[]) {
   std::cout << "*            This is not a haiku                                 *" << std::endl;
   std::cout << "*                                                                *" << std::endl;
   std::cout << "*                                                                *" << std::endl;
-  std::cout << "* Authors: G. V. Barroso                 Last Modif. 09/Nov/2023 *" << std::endl;
+  std::cout << "* Authors: G. V. Barroso                 Last Modif. 22/Nov/2023 *" << std::endl;
   std::cout << "*          A. P. Ragsdale                                        *" << std::endl;
   std::cout << "*                                                                *" << std::endl;
   std::cout << "******************************************************************" << std::endl;
@@ -401,25 +87,24 @@ int main(int argc, char *argv[]) {
 
   gsl_rng_set(gen, seed);
 
-  std::vector<double> Xl(0);
-  std::vector<double> Xr(0);
-  std::vector<std::array<double, 4>> X(0);
-
-  X.reserve(10 * L);
-  Xl.reserve(10 * L);
-  Xr.reserve(10 * L);
+  TwoLocusPop root(0, L, Ne);
 
   std::cout << "\nBurn-in (" << B << " generations)...";
   std::cout.flush();
 
   for(size_t i = 0; i < B; i++)
   {
-    evolve(X, Xl, Xr, Ne, u, r, L, s, gen);
-    cleanup(X, Xl, Xr);
+    root.evolve(u, r, s, gen);
+    root.cleanup();
   }
+
+  TwoLocusPop p1 = root;
+  //TwoLocusPop p2 = root;
 
   std::cout << "done.\n";
   std::cout << "\nEvolving population(s) (" << G << " generations)...\n";
+
+  // TODO multi-thread from root in 2-pops case
 
   double sum_Hl = 0.;
   double sum_Hr = 0.;
@@ -429,16 +114,16 @@ int main(int argc, char *argv[]) {
 
   for(size_t i = 0; i < G; ++i)
   {
-    evolve(X, Xl, Xr, Ne, L, u, r, s, gen);
-    cleanup(X, Xl, Xr);
+    p1.evolve(u, r, s, gen);
+    p1.cleanup();
 
-    sum_Hl += getSumHl(Xl);
-    sum_Hr += getSumHr(Xr);
-    sum_Dz += getSumDz(X);
-    sum_Dsqr += getSumDsqr(X);
-    sum_pi2 += getSumPi2(X);
+    sum_Hl += p1.getSumHl();
+    sum_Hr += p1.getSumHr();
+    sum_Dz += p1.getSumDz();
+    sum_Dsqr += p1.getSumDsqr();
+    sum_pi2 += p1.getSumPi2();
 
-    if(i % Ne == 1)
+    if(i % (10 * Ne) == 1)
     {
       std::cout << "Generation " << i << "\n";
       std::cout << "\tavg_Hl = " << sum_Hl / L / i << "\n";
