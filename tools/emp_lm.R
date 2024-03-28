b05b486138f301d0e463c0ce8613fcd3d3bce717
Gustavo V. Barroso <gvbarroso@gmail.com>
2024-03-28 16:51:22 -0500

    improves empirical lms

--- tools/emp_lm.R
+++ tools/emp_lm.R
@@ -14,117 +14,111 @@ suppressMessages({
   library(nlme)
   library(car)
   library(data.table)
+  library(ppcor)
 })
 
 print(Sys.time())
 cat("Fitting linear models pi ~ u + B...\n")
 
-chr_list <- c(1:4, 17:22)
+chr_list <- c(1:22)
 
 tbl_10kb <- data.frame()
 for(i in chr_list) {
   name <- paste("chr", i, ".10kb.tsv", sep="")
-  tbl <- fread(paste("primate_data/", name, sep=""))
+  tbl <- fread(paste("SeplyarskiyEtAl2023/", name, sep=""))
   tbl$bin <- 1:nrow(tbl)
   tbl$chr <- i
   tbl_10kb <- rbind.data.frame(tbl_10kb, tbl)
 }
+names(tbl_10kb)[5] <- "diversity"
 
 tbl_100kb <- data.frame()
 for(i in chr_list) {
   name <- paste("chr", i, ".100kb.tsv", sep="")
-  tbl <- fread(paste("primate_data/", name, sep=""))
+  tbl <- fread(paste("SeplyarskiyEtAl2023/", name, sep=""))
   tbl$bin <- 1:nrow(tbl)
   tbl$chr <- i
   tbl_100kb <- rbind.data.frame(tbl_100kb, tbl)
 }
+names(tbl_100kb)[5] <- "diversity"
 
 tbl_1Mb <- data.frame()
 for(i in chr_list) {
   name <- paste("chr", i, ".1Mb.tsv", sep="")
-  tbl <- fread(paste("primate_data/", name, sep=""))
+  tbl <- fread(paste("SeplyarskiyEtAl2023/", name, sep=""))
   tbl$bin <- 1:nrow(tbl)
   tbl$chr <- i
   tbl_1Mb <- rbind.data.frame(tbl_1Mb, tbl)
 }
+names(tbl_1Mb)[5] <- "diversity"
 
 # standardizing variables to help interpretation of linear coefficients
 ## 10 kb
-std_10kb <- dplyr::select(tbl_10kb, c(mu, r, pi, sub))
-names(std_10kb)[3] <- "diversity"
-std_10kb <- as.data.frame(apply(std_10kb[,1:ncol(std_10kb)], 2,
-              function(x) (x-mean(x, na.rm=T)) / sd(x, na.rm=T)))
-std_10kb$bin <- tbl_10kb$bin
-std_10kb$chr <- tbl_10kb$chr
+x10kb <- na.omit(tbl_10kb)
+x10kb$cum_bin <- 1:nrow(x10kb)
+x10kb$std_sub <- (x10kb$sub-mean(x10kb$sub)) / sd(x10kb$sub)
+x10kb$std_pi <- (x10kb$diversity-mean(x10kb$diversity)) / sd(x10kb$diversity)
+x10kb$std_mu <- (x10kb$mu-mean(x10kb$mu)) / sd(x10kb$mu)
+x10kb$std_r <- (x10kb$r-mean(x10kb$r)) / sd(x10kb$r)
 
 ## 100 kb
-std_100kb <- dplyr::select(tbl_100kb, c(mu, r, pi, sub))
-names(std_100kb)[3] <- "diversity"
-std_100kb <- as.data.frame(apply(std_100kb[,1:ncol(std_100kb)], 2,
-                function(x) (x-mean(x, na.rm=T)) / sd(x, na.rm=T)))
-std_100kb$bin <- tbl_100kb$bin
-std_100kb$chr <- tbl_100kb$chr
+x100kb <- na.omit(tbl_100kb)
+x100kb$cum_bin <- 1:nrow(x100kb)
+x100kb$std_sub <- (x100kb$sub-mean(x100kb$sub)) / sd(x100kb$sub)
+x100kb$std_pi <- (x100kb$diversity-mean(x100kb$diversity)) / sd(x100kb$diversity)
+x100kb$std_mu <- (x100kb$mu-mean(x100kb$mu)) / sd(x100kb$mu)
+x100kb$std_r <- (x100kb$r-mean(x100kb$r)) / sd(x100kb$r)
 
 ## 1 Mb
-std_1Mb <- dplyr::select(tbl_1Mb, c(mu, r, pi, sub))
-names(std_1Mb)[3] <- "diversity"
-std_1Mb <- as.data.frame(apply(std_1Mb[,1:ncol(std_1Mb)], 2,
-              function(x) (x-mean(x, na.rm=T)) / sd(x, na.rm=T)))
-std_1Mb$bin <- tbl_1Mb$bin
-std_1Mb$chr <- tbl_1Mb$chr
+x1Mb <- na.omit(tbl_1Mb)
+x1Mb$cum_bin <- 1:nrow(x1Mb)
+x1Mb$std_sub <- (x1Mb$sub-mean(x1Mb$sub)) / sd(x1Mb$sub)
+x1Mb$std_pi <- (x1Mb$diversity-mean(x1Mb$diversity)) / sd(x1Mb$diversity)
+x1Mb$std_mu <- (x1Mb$mu-mean(x1Mb$mu)) / sd(x1Mb$mu)
+x1Mb$std_r <- (x1Mb$r-mean(x1Mb$r)) / sd(x1Mb$r)
 
-# linear models
-## tables for storing coefficients from linear models
-beta_pi_10kb <- as.data.frame(matrix(ncol=2, nrow=length(chr_list)))
-names(beta_pi_10kb) <- c("r", "u")
-beta_pi_10kb$chr <- chr_list
-beta_sub_10kb <- beta_pi_10kb
+m_1Mb <- pivot_longer(x1Mb, cols=c("mu", "r", "diversity", "sub"), names_to="var")
+ggplot(data=filter(m_1Mb, var=="diversity"),
+       aes(x=cum_bin, y=value, color=as.factor(chr))) +
+geom_point() + theme_bw() + geom_line() + 
+  scale_x_continuous(breaks=pretty_breaks()) +
+  scale_y_continuous(breaks=pretty_breaks()) + 
+  labs(title="Diversity across human chromosomes (1Mb windows)",
+       x="Cummulative Bin", y=expression(pi)) +
+  theme(axis.title=element_text(size=16), 
+        axis.text=element_text(size=12), 
+        axis.text.x=element_text(size=12),
+        legend.text=element_text(size=16),
+        legend.title=element_text(size=16),
+        legend.position="none")
 
-beta_pi_100kb <- as.data.frame(matrix(ncol=2, nrow=length(chr_list)))
-names(beta_pi_100kb) <- c("r", "u")
-beta_pi_100kb$chr <- chr_list
-beta_sub_100kb <- beta_pi_100kb
+# diversity
+gls.pi.100kb.genome <- gls(std_pi ~ std_mu + std_r, data=x100kb,
+                         cor=corAR1(0, ~cum_bin|chr),
+                         weights=varPower(0, ~chr),
+                         method="ML")
+summary(gls.pi.100kb.genome)
+vif(gls.pi.100kb.genome)
 
-beta_pi_1Mb <- as.data.frame(matrix(ncol=2, nrow=length(chr_list)))
-names(beta_pi_1Mb) <- c("r", "u")
-beta_pi_1Mb$chr <- chr_list
-beta_sub_1Mb <- beta_pi_1Mb
+gls.pi.1Mb.genome <- gls(std_pi ~ std_mu + std_r, data=x1Mb,
+                    cor=corAR1(0, ~cum_bin|chr),
+                    weights=varPower(0, ~chr),
+                    method="ML")
+summary(gls.pi.1Mb.genome)
+vif(gls.pi.1Mb.genome)
 
-pb <- txtProgressBar(min=1, max=length(chr_list) + 1, style=3)
-for(i in 1:length(chr_list)) {
-  
-  setTxtProgressBar(pb, i)
-  
-  # linear models on pi
-  #gls.pi.10kb <- gls(diversity ~ mu + r, data=filter(std_10kb, chr==chr_list[i]),
-  #                  cor=corAR1(0, ~bin), method="ML", na.action=na.omit)
-  #beta_pi_10kb$u[i] <- gls.pi.10kb$coefficients[2]
-  #beta_pi_10kb$r[i] <- gls.pi.10kb$coefficients[3]
-  
-  gls.pi.100kb <- gls(diversity ~ mu + r, data=na.omit(filter(std_100kb, chr==chr_list[i])),
-                    cor=corAR1(0, ~bin), method="ML", na.action=na.omit, verbose=T)
-  beta_pi_100kb$u[i] <- gls.pi.100kb$coefficients[2]
-  beta_pi_100kb$r[i] <- gls.pi.100kb$coefficients[3]
-  
-  gls.pi.1Mb <- gls(diversity ~ mu + r, data=filter(std_1Mb, chr==chr_list[i]),
-                    cor=corAR1(0, ~bin), method="ML", na.action=na.omit)
-  beta_pi_1Mb$u[i] <- gls.pi.1Mb$coefficients[2]
-  beta_pi_1Mb$r[i] <- gls.pi.1Mb$coefficients[3]
-  
-  # linear models on divergence
-  #gls.sub.10kb <- gls(sub ~ mu + r, data=filter(std_10kb, chr==chr_list[i]),
-  #                    cor=corAR1(0, ~bin), method="ML", na.action=na.omit)
-  #beta_sub_10kb$u[i] <- gls.sub.10kb$coefficients[2]
-  #beta_sub_10kb$r[i] <- gls.sub.10kb$coefficients[3]
-  
-  gls.sub.100kb <- gls(sub ~ mu + r, data=filter(std_100kb, chr==chr_list[i]),
-                       cor=corAR1(0, ~bin), method="ML", na.action=na.omit)
-  beta_sub_100kb$u[i] <- gls.sub.100kb$coefficients[2]
-  beta_sub_100kb$r[i] <- gls.sub.100kb$coefficients[3]
-  
-  gls.sub.1Mb <- gls(sub ~ mu + r, data=filter(std_1Mb, chr==chr_list[i]),
-                     cor=corAR1(0, ~bin), method="ML", na.action=na.omit)
-  beta_sub_1Mb$u[i] <- gls.sub.1Mb$coefficients[2]
-  beta_sub_1Mb$r[i] <- gls.sub.1Mb$coefficients[3]
-}
-close(pb)
+# divergence
+gls.sub.100kb.genome <- gls(std_sub ~ std_mu + std_r, data=x100kb,
+                           cor=corAR1(0, ~cum_bin|chr),
+                           weights=varPower(0, ~chr),
+                           method="ML")
+summary(gls.sub.100kb.genome)
+vif(gls.sub.100kb.genome)
+
+gls.sub.1Mb.genome <- gls(std_sub ~ std_mu + std_r, data=x1Mb,
+                         cor=corAR1(0, ~cum_bin|chr),
+                         weights=varPower(0, ~chr),
+                         method="ML")
+summary(gls.sub.1Mb.genome)
+vif(gls.sub.1Mb.genome)
+
