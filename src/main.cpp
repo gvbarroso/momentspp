5c8e41ac372d46fdc63ddcc75c531c4215536678
Gustavo V. Barroso <gvbarroso@gmail.com>
2025-04-17 15:52:49 -0500

    improves causal_model.Rmd

--- src/main.cpp
+++ src/main.cpp
@@ -52,8 +52,8 @@ int main(int argc, char *argv[]) {
 
   if(argc == 1)
   {
-    std::cout << "Usage:\n\n";
-    std::cout << "momentspp params=opt.bpp\n";
+    std::cout << "Usage:\n";
+    std::cout << "momentspp param=opt.bpp\n\n";
 
     std::cout << "\nThe github repository contains instructions on how to write the options file:\n";
     std::cout << "https://github.com/gvbarroso/momentspp/tree/main/doc" << std::endl << std::endl;
