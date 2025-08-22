7f39369a17b166145e8ffb9a8af7b2184b47a8ca
gvbarroso <gvbarroso@gmail.com>
2025-08-22 14:49:36 +0200

    minor

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
   std::cout << "*            Moment by moment                                    *" << std::endl;
   std::cout << "*                                                                *" << std::endl;
   std::cout << "*                                                                *" << std::endl;
-  std::cout << "* Authors: G. V. Barroso                 Last Modif. 14/Mar/2024 *" << std::endl;
+  std::cout << "* Authors: G. V. Barroso                 Last Modif. 14/Mar/2025 *" << std::endl;
   std::cout << "*          A. P. Ragsdale                                        *" << std::endl;
   std::cout << "*                                                                *" << std::endl;
   std::cout << "******************************************************************" << std::endl;
